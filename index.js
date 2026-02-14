ea532a6657b40d7f67cbe9dc4b00724358ed4990
muruga42 <venkadeshmuruga@gmail.com>
2026-02-14 09:41:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const app = express();
 
 app.use(express.json());
 
+// Import routes
+const userRoutes = require("./routes/userRoutes");
+const adminRoutes = require("./routes/adminRoutes");
+
+// Use routes
+app.use("/api/users", userRoutes);
+app.use("/api/admin", adminRoutes);
+
+// Root test
 app.get("/", (req, res) => {
   res.send("Backend is running successfully ✅");
 });
